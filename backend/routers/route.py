0bc3be525badd514f7e2531277db589c8235e0f4
kaito <kaito1013kaito@gmail.com>
2024-03-06 01:27:57 +0900

    ✨ postしたときにGoogleChatを返すように変更

--- backend/routers/route.py
+++ backend/routers/route.py
@@ -6,6 +6,8 @@ import schemas.history as history_schema
 import schemas.chat as model_schema
 import schemas.chat as question_schema
 
+from LangChain.GoogleChat import Chat_GoogleAI
+
 router = APIRouter()
 
 @router.get("/history/",response_model=List[history_schema.History])
@@ -13,6 +15,8 @@ async def history():
     pass
 
 @router.post("/chat/",response_model=List[chat_schema.Chat])
-async def chat(question:question_schema.Question,model:List[model_schema.Model]):
-    return [chat_schema.Chat(answer="Hello"),chat_schema.Chat(answer="World")]
+# async def chat(question:question_schema.Question,model:List[model_schema.Model]):
+async def chat(question:question_schema.Question):
+    ans=Chat_GoogleAI("gemini-pro",1.0,1.0,question.question)
+    return [chat_schema.Chat(answer=ans)]
 
