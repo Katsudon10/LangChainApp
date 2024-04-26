ad92f7a25827d7f5e67179e5ef6b35d2709329c6
kaito <kaito1013kaito@gmail.com>
2024-04-26 16:15:57 +0900

    ✨ backend/LangChain/Anthropic.pyを追加

---
+++ backend/LangChain/Anthropic.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+
+from langchain_anthropic import ChatAnthropic
+
+load_dotenv()
+
+os.environ["ANTHROPIC_API_KEY"] = os.getenv("ANTHROPIC_API_KEY")
+
+def Chat_Anthropic(model_name,temperature,top_p, question):
+    model = ChatAnthropic(
+        model_name=model_name,
+        temperature=temperature,
+        kwargs={
+            "top_p": top_p,
+        }
+    )
+
+    answer = model.invoke(question).content
+    return answer
+
+if __name__ == "__main__":
+    # model_name = "claude-3-opus-20240229"
+    # model_name = "claude-3-sonnet-20240229"
+    # model_name = "claude-3-haiku-20240307"
+
+    print(Chat_Azure("gpt-35-turbo",1.0,0.7, "What is the meaning of life?"))
