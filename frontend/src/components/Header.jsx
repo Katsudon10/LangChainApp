18192665dbfd3cf1e72589d1a80d1acb016f36fa
kaito <kaito1013kaito@gmail.com>
2024-09-02 17:46:13 +0900

    fix header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className="fixed top-0 right-0 left-0 h-16 bg-cyan-300 my-auto">
+            <h1 className="text-lg p-4">LLM-Orchester</h1>
+        </div>
+    );
+};
+
+export default Header;
