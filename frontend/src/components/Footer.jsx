bd6c3ca148ae31e1ad000934246f8a7e48c33383
kaito <kaito1013kaito@gmail.com>
2024-09-04 02:11:44 +0900

    Update footer and header styles

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer= () => {
   return (
-    <footer className="fixed bottom-0 right-0 left-0">
+    <footer className="fixed bottom-0 right-0 left-0 text-center">
       <p>© 2024 kaito inoue.</p>
     </footer>
   );
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <div className="fixed top-0 right-0 left-0 h-16 bg-cyan-300 my-auto">
-            <h1 className="text-lg p-4">LLM-Orchester</h1>
+            <h1 className="text-lg p-4 text-center">LLM-Orchester</h1>
         </div>
     );
 };
