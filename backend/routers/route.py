218f03f6afa9b8e8ac73c87dea03d0fd48221dfc
kaito <kaito1013kaito@gmail.com>
2024-09-04 02:13:07 +0900

    Add Anthropic chat functionality and update OpenAI model version

--- backend/routers/route.py
+++ backend/routers/route.py
@@ -8,7 +8,8 @@ import schemas.chat as question_schema
 
 from LangChain.GoogleChat import Chat_GoogleAI
 # from LangChain.AzureChat import Chat_Azure
-# from LangChain.OpenAIChat import Chat_OpenAI
+from LangChain.OpenAIChat import Chat_OpenAI
+from LangChain.Anthropic import Chat_Anthropic
 
 router = APIRouter()
 
@@ -19,12 +20,14 @@ async def history():
 @router.post("/chat/",response_model=List[chat_schema.Chat])
 # async def chat(question:question_schema.Question,model:List[model_schema.Model]):
 async def chat(question:question_schema.Question):
-    # ans_google=Chat_GoogleAI("gemini-pro",1.0,1.0,question.question)
+    ans_google=Chat_GoogleAI("gemini-1.5-flash",1.0,1.0,question.question)
     # ans_azure=Chat_Azure("gpt-35-turbo",1.0,1.0,question.question)
-    # ans_openai=Chat_OpenAI("gpt-3.5-turbo",1.0,1.0,question.question)
-    # retrun [chat_schema.Chat(answer=ans_openai),chat_schema.Chat(answer=ans_azure),chat_schema.Chat(answer=ans_google)]
+    ans_openai=Chat_OpenAI("gpt-4o",1.0,1.0,question.question)
+    ans_anthropic=Chat_Anthropic("claude-3-haiku-20240307",1.0,1.0,question.question)
+    # retrun [chat_schema.Chat(answer=ans_openai),chat_schema.Chat(answer=ans_azure),chat_schema.Chat(answer=ans_google),chat_schema.Chat(answer=ans_anthropic)]
+    return [chat_schema.Chat(answer=ans_openai),chat_schema.Chat(answer=ans_google),chat_schema.Chat(answer=ans_anthropic)]
     
-    ans=Chat_GoogleAI("gemini-pro",1.0,1.0,question.question)
-    return [chat_schema.Chat(answer=ans),chat_schema.Chat(answer=ans),chat_schema.Chat(answer=ans)]
+    # ans=Chat_GoogleAI("gemini-pro",1.0,1.0,question.question)
+    # return [chat_schema.Chat(answer=ans),chat_schema.Chat(answer=ans),chat_schema.Chat(answer=ans)]
     
 
